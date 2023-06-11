4b34c2172321c7075031eb44f0c463c00df52234
timdoolan <tim.doolan88@icloud.com>
2023-06-11 22:25:30 +0100

    feat: setup onboarding navigator and flow

--- app/navigators/AppNavigator.tsx
+++ app/navigators/AppNavigator.tsx
@@ -1,9 +1,4 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  NavigationContainer,
-  NavigatorScreenParams,
-} from "@react-navigation/native"
+import { DarkTheme, DefaultTheme, NavigationContainer, NavigatorScreenParams } from "@react-navigation/native"
 import { createNativeStackNavigator, NativeStackScreenProps } from "@react-navigation/native-stack"
 import { observer } from "mobx-react-lite"
 import React from "react"
@@ -17,26 +12,24 @@ import { colors } from "app/theme"
 
 export type AppStackParamList = {
   Welcome: undefined
+  Onboarding: undefined
+  Register: undefined
   Login: undefined
   Demo: NavigatorScreenParams<DemoTabParamList>
 }
 
 const exitRoutes = Config.exitRoutes
 
-export type AppStackScreenProps<T extends keyof AppStackParamList> = NativeStackScreenProps<
-  AppStackParamList,
-  T
->
+export type AppStackScreenProps<T extends keyof AppStackParamList> = NativeStackScreenProps<AppStackParamList, T>
 
 const Stack = createNativeStackNavigator<AppStackParamList>()
 
 const OnboardingStack = observer(function OnboardingStack() {
   return (
-    <Stack.Navigator
-      screenOptions={{ headerShown: false, navigationBarColor: colors.background }}
-      initialRouteName="Welcome"
-    >
+    <Stack.Navigator screenOptions={{ headerShown: false, navigationBarColor: colors.background }} initialRouteName="Welcome">
       <Stack.Screen name="Welcome" component={Screens.WelcomeScreen} />
+      <Stack.Screen name="Onboarding" component={Screens.OnboardingScreen} />
+      <Stack.Screen name="Register" component={Screens.RegisterScreen} />
     </Stack.Navigator>
   )
 })
@@ -64,22 +57,23 @@ const AppStack = observer(function AppStack() {
   )
 })
 
-export interface NavigationProps
-  extends Partial<React.ComponentProps<typeof NavigationContainer>> {}
+export interface NavigationProps extends Partial<React.ComponentProps<typeof NavigationContainer>> {}
 
 export const AppNavigator = observer(function AppNavigator(props: NavigationProps) {
   const colorScheme = useColorScheme()
-  const onboardingIsComplete = false
+
+  const {
+    onboardingStore: { isOnboardingComplete },
+    registrationStore: { isRegistrationComplete },
+  } = useStores()
+
+  const showApp = isOnboardingComplete && isRegistrationComplete
 
   useBackButtonHandler((routeName) => exitRoutes.includes(routeName))
 
   return (
-    <NavigationContainer
-      ref={navigationRef}
-      theme={colorScheme === "dark" ? DarkTheme : DefaultTheme}
-      {...props}
-    >
-      {onboardingIsComplete ? <AppStack /> : <OnboardingStack />  }
+    <NavigationContainer ref={navigationRef} theme={colorScheme === "dark" ? DarkTheme : DefaultTheme} {...props}>
+      {showApp ? <AppStack /> : <OnboardingStack />}
     </NavigationContainer>
   )
 })
--- app/screens/WelcomeScreen.tsx
+++ app/screens/WelcomeScreen.tsx
@@ -6,6 +6,7 @@ import { AppStackScreenProps } from "../navigators"
 import { colors, spacing } from "../theme"
 import { useHeader } from "../utils/useHeader"
 import { useSafeAreaInsetsStyle } from "../utils/useSafeAreaInsetsStyle"
+import { useStores } from "../models"
 
 const welcomeLogo = require("../../assets/images/logo.png")
 
@@ -13,18 +14,21 @@ interface WelcomeScreenProps extends AppStackScreenProps<"Welcome"> {}
 
 export const WelcomeScreen: FC<WelcomeScreenProps> = observer(function WelcomeScreen(_props) {
   const { navigation } = _props
-
+  const {
+    onboardingStore: { setOnboardingComplete },
+  } = useStores()
   function goNext() {
-    navigation.navigate("WalkThrough", { screen: "WalkThrough" })
+    navigation.navigate("Onboarding")
   }
 
-  function goRegister() {
-    navigation.navigate("Regsiter", { screen: "Regsiter" })
+  function skip() {
+    setOnboardingComplete(true)
+    navigation.navigate("Register")
   }
 
   useHeader({
     rightTx: "common.skip",
-    onRightPress: goRegister,
+    onRightPress: skip,
   })
 
   const $bottomContainerInsets = useSafeAreaInsetsStyle(["bottom"])
@@ -33,23 +37,13 @@ export const WelcomeScreen: FC<WelcomeScreenProps> = observer(function WelcomeSc
     <View style={$container}>
       <View style={$topContainer}>
         <Image style={$welcomeLogo} source={welcomeLogo} resizeMode="contain" />
-        <Text
-          testID="welcome-heading"
-          style={$welcomeHeading}
-          tx="welcomeScreen.welcomeMessage"
-          preset="heading"
-        />
+        <Text testID="welcome-heading" style={$welcomeHeading} tx="welcomeScreen.heading" preset="heading" />
         <Text tx="welcomeScreen.eVeSlogan" preset="subheading" />
       </View>
 
       <View style={[$bottomContainer, $bottomContainerInsets]}>
         <Text tx="welcomeScreen.postscript" size="md" />
-        <Button
-          testID="next-screen-button"
-          preset="reversed"
-          tx="welcomeScreen.letsGo"
-          onPress={goNext}
-        />
+        <Button testID="next-screen-button" preset="reversed" tx="welcomeScreen.letsGo" onPress={goNext} />
       </View>
     </View>
   )
