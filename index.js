88156d267bcba4eb5eed7a21c0fbedad2c9a0a95
timdoolan <tim.doolan88@icloud.com>
2023-08-09 14:56:08 +0100

    chore: remove demo comments

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-// This is the first file that ReactNative will run when it starts up.
-// If you use Expo (`yarn expo:start`), the entry point is ./App.js instead.
-// Both do essentially the same thing.
-
 import App from "./app/app.tsx"
 import React from "react"
 import { AppRegistry } from "react-native"
