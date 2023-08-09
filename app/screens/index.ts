de2f7058769722fe73b35bdc56783890ef6ad34f
timdoolan <tim.doolan88@icloud.com>
2023-08-09 14:56:01 +0100

    chore: remove demo comments

--- app/screens/index.ts
+++ app/screens/index.ts
@@ -1,11 +1,8 @@
 export * from "./WelcomeScreen"
-// @demo remove-block-start
 export * from "./LoginScreen"
 export * from "./ProfileScreen"
 export * from "./DebugScreen"
-export * from "./SearchScreen/SearchScreen"
-// @demo remove-block-end
+export * from "./SearchScreen"
 export * from "./ErrorScreen/ErrorBoundary"
-// export other screens here
 export * from "./OnboardingScreen"
 export * from "./RegisterScreen"
