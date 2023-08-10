c6757b2df16c99ab43c72566c2f89fb72278b5cb
timdoolan <tim.doolan88@icloud.com>
2023-08-10 21:35:37 +0100

    feat: add search icon

--- app/components/Icon.tsx
+++ app/components/Icon.tsx
@@ -107,6 +107,7 @@ export const iconRegistry = {
   slack: require("../../assets/icons/slack.png"),
   view: require("../../assets/icons/view.png"),
   x: require("../../assets/icons/x.png"),
+  search: require("../../assets/icons/search.png"),
 }
 
 const $imageStyle: ImageStyle = {
