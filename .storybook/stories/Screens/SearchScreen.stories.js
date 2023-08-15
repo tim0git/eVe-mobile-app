4d74742d8ce5437e3ebe2764f45a53bf7ccb2bfa
timdoolan <tim.doolan88@icloud.com>
2023-08-15 20:42:05 +0200

    refactor: mock locations query into location.ts

--- .storybook/stories/Screens/SearchScreen.stories.js
+++ .storybook/stories/Screens/SearchScreen.stories.js
@@ -3,34 +3,9 @@ import { SearchScreen } from "../../../app/screens"
 import { NavigationDecorator } from "../../decorators"
 import { storiesOf } from "@storybook/react-native"
 import ApolloDecorator from "../../decorators/ApolloProvider"
-import { GET_LOCATIONS } from "../../../app/services/graphql/queries/location"
-
-const mocks = [
-  {
-    request: {
-      query: GET_LOCATIONS,
-    },
-
-    result: {
-      data: {
-        locations: [
-          { id: "loc-1" },
-          { id: "loc-2" },
-          { id: "loc-3" },
-          { id: "loc-4" },
-          { id: "loc-5" },
-          { id: "loc-6" },
-          { id: "loc-7" },
-          { id: "loc-8" },
-          { id: "loc-9" },
-          { id: "loc-10" },
-        ],
-      },
-    },
-  },
-]
+import { GET_LOCATIONS_MOCK } from "../../../app/services/graphql"
 
 storiesOf("Screens/SearchScreen", module)
   .addDecorator(NavigationDecorator)
-  .addDecorator(ApolloDecorator({ mocks: mocks }))
+  .addDecorator(ApolloDecorator({ mocks: GET_LOCATIONS_MOCK }))
   .add("Default", () => <SearchScreen />)
--- app/services/graphql/queries/location.ts
+++ app/services/graphql/queries/location.ts
@@ -7,3 +7,28 @@ export const GET_LOCATIONS = gql`
     }
   }
 `
+
+export const GET_LOCATIONS_MOCK = [
+  {
+    request: {
+      query: GET_LOCATIONS,
+    },
+
+    result: {
+      data: {
+        locations: [
+          { id: "loc-1" },
+          { id: "loc-2" },
+          { id: "loc-3" },
+          { id: "loc-4" },
+          { id: "loc-5" },
+          { id: "loc-6" },
+          { id: "loc-7" },
+          { id: "loc-8" },
+          { id: "loc-9" },
+          { id: "loc-10" },
+        ],
+      },
+    },
+  },
+]
